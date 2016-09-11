b870cb136aa54650590358d76730fa2713a9b222
Marco Satti <marcosatti@gmail.com>
2016-09-11 21:31:44 +0800

    Forgot to check the COP0.Status IM2 bit before sending the exception to be processed. Also, having a hard time figuring out how to implement the external timers..

--- PCSX2_Core/Source/VM/ExecutionCore/Interpreter/InterpreterEE/INTCHandler/INTCHandler.cpp
+++ PCSX2_Core/Source/VM/ExecutionCore/Interpreter/InterpreterEE/INTCHandler/INTCHandler.cpp
@@ -5,6 +5,8 @@
 #include "Common/PS2Resources/PS2Resources_t.h"
 #include "Common/PS2Resources/EE/EE_t.h"
 #include "Common/PS2Resources/EE/EECore/EECore_t.h"
+#include "Common/PS2Resources/EE/EECore/COP0/COP0_t.h"
+#include "Common/PS2Resources/EE/EECore/COP0/Types/COP0_BitfieldRegisters_t.h"
 #include "Common/PS2Resources/Types/XORStorageObject/XORStorageObject_t.h"
 #include "Common/PS2Resources/EE/EECore/Exceptions/Exceptions_t.h"
 #include "Common/PS2Resources/EE/EECore/Exceptions/Types/EECoreException_t.h"
@@ -18,16 +20,20 @@ INTCHandler::INTCHandler(const VMMain* const vmMain) :
 
 void INTCHandler::checkInterrupt() const
 {
+	// If any of the I_STAT with logical AND I_MASK bits are 1, then an interrupt may be generated.
 	const u32 I_STAT = getVM()->getResources()->EE->EE_REGISTER_I_STAT->readWordU(0);
 	if (I_STAT > 0)
 	{
 		const u32 I_MASK = getVM()->getResources()->EE->EE_REGISTER_I_MASK->readWordU(0);
 		if ((I_STAT & I_MASK) > 0)
 		{
-			// Generate an INT0 signal/interrupt exception.
-			auto& ExceptionQueue = getVM()->getResources()->EE->EECore->Exceptions->ExceptionQueue;
-			IntExceptionInfo_t intInfo = { 0, 1, 0 };
-			ExceptionQueue->push(EECoreException_t(ExType::EX_INTERRUPT, nullptr, &intInfo, nullptr));
+			// Generate an INT0 signal/interrupt exception, if the EE Core has the COP0.Status.IM[2] mask set.
+			if ((getVM()->getResources()->EE->EECore->COP0->Status->getFieldValue(RegisterStatus_t::Fields::IM) & 1) > 0)
+			{
+				auto& ExceptionQueue = getVM()->getResources()->EE->EECore->Exceptions->ExceptionQueue;
+				IntExceptionInfo_t intInfo = { 0, 1, 0 };
+				ExceptionQueue->push(EECoreException_t(ExType::EX_INTERRUPT, nullptr, &intInfo, nullptr));
+			}
 		}
 	}
 }
