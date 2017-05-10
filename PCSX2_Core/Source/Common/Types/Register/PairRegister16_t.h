9599fc3f7c5baa5b80d84659a2144e618c76e6e0
Marco Satti <marcosatti@gmail.com>
2017-05-10 08:06:10 +0800

    Removed inc/dec function from pair register... It can be done through read/write word easily enough.

--- PCSX2_Core/Source/Common/Types/Register/PairRegister16_t.h
+++ PCSX2_Core/Source/Common/Types/Register/PairRegister16_t.h
@@ -6,16 +6,15 @@
 
 /*
 A (low) 16-bit register to be used as part of a "pair" with another Register16_t (high) to make a hybrid 32-bit register.
-This is essentially a wrapper class that provides ways to:
- 1. Increment/decrement with overflow into the high register.
- 2. Read and write 32-bit values (similar a "word", disregarding the PS2 context), taking care of the splitting.
+This is essentially a wrapper class that provides ways to read and write 32-bit values (similar a "word", disregarding the PS2 context), taking care of the splitting.
 
 Usage example:
 // Create objects.
 std::shared_ptr<Register16_t> mHighRegister;
 std::shared_ptr<PairRegister16_t> mLowRegister;
 
-// Use low register to manipulate a 32-bit value or inc/decrement.
+// Use only the low register to manipulate a 32-bit value or inc/decrement. 
+// Any changes directly to the high register will be reflected in the functions defined.
 mLowRegister->writeWord(getContext(), 0x1234ABCD);
 u16 highValue = mHighRegister->readHword(getContext()); // Equal to 0x1234.
 u16 lowValue = mLowRegister->readHword(getContext()); // Equal to 0xABCD.
@@ -30,12 +29,6 @@ public:
 	*/
 	virtual u32 readWord(const System_t context);
 	virtual void writeWord(const System_t context, u32 value);
-
-	/*
-	Increment or decrement the register pair, which automatically takes care of over or underflow.
-	*/
-	virtual void increment(const u32 count);
-	virtual void decrement(const u32 count);
 
 private:
 	/*
