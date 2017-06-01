ec1752182c3245cd1e7b853db12237ba6561113a
Marco Satti <marcosatti@gmail.com>
2017-06-01 08:31:49 +0800

    WIP

--- cxx_flag_overrides_msvc.cmake
+++ cxx_flag_overrides_msvc.cmake
@@ -1,8 +1,8 @@
 if(MSVC)
 	# Build with static runtime libraries.
 	# See https://cmake.org/Wiki/CMake_FAQ#How_can_I_build_my_MSVC_application_with_a_static_runtime.3F
-	set(CMAKE_CXX_FLAGS_DEBUG_INIT "/D_DEBUG /MTd /Zi /Ob0 /Od /RTC1")
-	set(CMAKE_CXX_FLAGS_MINSIZEREL_INIT     "/MT /O1 /Ob1 /D NDEBUG")
-	set(CMAKE_CXX_FLAGS_RELEASE_INIT        "/MT /O2 /Ob2 /D NDEBUG")
-	set(CMAKE_CXX_FLAGS_RELWITHDEBINFO_INIT "/MT /Zi /O2 /Ob1 /D NDEBUG")
+	set(CMAKE_CXX_FLAGS_DEBUG_INIT          "/MTd /Zi /Ob0 /Od  /RTC1 /D_DEBUG  /MP")
+	set(CMAKE_CXX_FLAGS_MINSIZEREL_INIT     "/MT  /O1 /Ob1            /D NDEBUG /MP")
+	set(CMAKE_CXX_FLAGS_RELEASE_INIT        "/MT  /O2 /Ob2            /D NDEBUG /MP")
+	set(CMAKE_CXX_FLAGS_RELWITHDEBINFO_INIT "/MT  /Zi /O2  /Ob1       /D NDEBUG /MP")
 endif()
