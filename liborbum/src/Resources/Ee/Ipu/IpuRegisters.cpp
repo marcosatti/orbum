213fe2fd0aff1179024c5998ca1f296c4b4375f1
hch12907 <haochengho12907@gmail.com>
2018-07-12 20:53:18 +0800

    Detail IPU_CTRL register and fix bug
    - Thought that Bitfields are constructed with (start, end), but it's actually (start, length). Oops.
    - I should rewrite those `is_busy()`s for better code reuse...

--- liborbum/src/Resources/Ee/Ipu/IpuRegisters.cpp
+++ liborbum/src/Resources/Ee/Ipu/IpuRegisters.cpp
@@ -23,3 +23,15 @@ void IpuRegister_Top::set_busy()
     udword cmd = this->read_udword();
     this->write_udword(this->BUSY.insert_into(cmd, (udword)1));
 }
+
+bool IpuRegister_Ctrl::is_busy() 
+{
+    udword cmd = this->read_uword();
+    return (this->BUSY.extract_from(cmd) == 1);
+}
+
+void IpuRegister_Ctrl::set_busy() 
+{
+    udword cmd = this->read_uword();
+    this->write_uword(this->BUSY.insert_into(cmd, (udword)1));
+}
--- liborbum/src/Resources/Ee/Ipu/IpuRegisters.hpp
+++ liborbum/src/Resources/Ee/Ipu/IpuRegisters.hpp
@@ -1,12 +1,13 @@
 #include "Common/Types/Register/SizedDwordRegister.hpp"
+#include "Common/Types/Register/SizedWordRegister.hpp"
 
 class IpuRegister_Cmd : public SizedDwordRegister 
 {
 public:
-    static constexpr Bitfield BUSY = Bitfield(63, 63);
-    static constexpr Bitfield CODE = Bitfield(28, 31);
-    static constexpr Bitfield DATA = Bitfield(0, 31);
-    static constexpr Bitfield OPTION = Bitfield(0, 27);
+    static constexpr Bitfield BUSY = Bitfield(63, 1);
+    static constexpr Bitfield CODE = Bitfield(28, 4);
+    static constexpr Bitfield DATA = Bitfield(0, 32);
+    static constexpr Bitfield OPTION = Bitfield(0, 28);
 
     // Note: I actually preferred the is_* methods to be `const`.
 
@@ -18,14 +19,34 @@ public:
     void set_busy();
 };
 
-
 class IpuRegister_Top : public SizedDwordRegister 
 {
 public:
-    static constexpr Bitfield BSTOP = Bitfield(0, 31);
-    static constexpr Bitfield BUSY  = Bitfield(63, 63);
+    static constexpr Bitfield BSTOP = Bitfield(0, 32);
+    static constexpr Bitfield BUSY  = Bitfield(63, 1);
 
     /// BSTOP field is ineffective if BUSY is set to 1.
     bool is_busy();
     void set_busy();
-}
+};
+
+class IpuRegister_Ctrl : public SizedWordRegister 
+{
+public:
+    static constexpr Bitfield IFC = Bitfield(0, 4);
+    static constexpr Bitfield OFC = Bitfield(4, 4);
+    static constexpr Bitfield CBP = Bitfield(8, 6);
+    static constexpr Bitfield ECD = Bitfield(14, 1);
+    static constexpr Bitfield SCD = Bitfield(15, 1);
+    static constexpr Bitfield IDP = Bitfield(16, 2);
+    static constexpr Bitfield AS  = Bitfield(20, 1);
+    static constexpr Bitfield IVF = Bitfield(21, 1);
+    static constexpr Bitfield QST = Bitfield(22, 1);
+    static constexpr Bitfield MP1 = Bitfield(23, 1);
+    static constexpr Bitfield PCT = Bitfield(24, 3);
+    static constexpr Bitfield RST = Bitfield(30, 1);
+    static constexpr Bitfield BUSY = Bitfield(31, 1);
+
+    bool is_busy();
+    void set_busy();
+};
