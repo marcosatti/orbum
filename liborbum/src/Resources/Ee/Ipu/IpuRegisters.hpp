2e6dab43124d8f42805a0f0926097494d1d6e5be
hch12907 <haochengho12907@gmail.com>
2018-07-15 01:42:29 +0800

    Remove all *_busy() methods
    in the end I think it is redundant

--- liborbum/src/Resources/Ee/Ipu/IpuRegisters.hpp
+++ liborbum/src/Resources/Ee/Ipu/IpuRegisters.hpp
@@ -1,33 +1,24 @@
 #include "Common/Types/Register/SizedDwordRegister.hpp"
 #include "Common/Types/Register/SizedWordRegister.hpp"
 
+/// Refer to EE User's Manual pg 183 for the registers.
 class IpuRegister_Cmd : public SizedDwordRegister 
 {
 public:
+    /// BUSY field indicates the progress of the current decoding process.
+    /// When set to 1, DATA field is meaningless.
     static constexpr Bitfield BUSY = Bitfield(63, 1);
     static constexpr Bitfield CODE = Bitfield(28, 4);
     static constexpr Bitfield DATA = Bitfield(0, 32);
-    static constexpr Bitfield OPTION = Bitfield(0, 28);
-
-    // Note: I actually preferred the is_* methods to be `const`.
-
-    /// When reading, bit 63 serves as the BUSY bit, which denotes the progress of
-    /// the decoding process. When it is set to 1, the decoding process is still
-    /// being executed.
-    /// Refer to EE User's Manual pg 183.
-    bool is_busy();
-    void set_busy();
+    static constexpr Bitfield OPTION = Bitfield(0, 28);  
 };
 
 class IpuRegister_Top : public SizedDwordRegister 
 {
 public:
-    static constexpr Bitfield BSTOP = Bitfield(0, 32);
-    static constexpr Bitfield BUSY  = Bitfield(63, 1);
-
     /// BSTOP field is ineffective if BUSY is set to 1.
-    bool is_busy();
-    void set_busy();
+    static constexpr Bitfield BUSY  = Bitfield(63, 1);
+    static constexpr Bitfield BSTOP = Bitfield(0, 32);
 };
 
 class IpuRegister_Ctrl : public SizedWordRegister 
@@ -46,9 +37,6 @@ public:
     static constexpr Bitfield PCT = Bitfield(24, 3);
     static constexpr Bitfield RST = Bitfield(30, 1);
     static constexpr Bitfield BUSY = Bitfield(31, 1);
-
-    bool is_busy();
-    void set_busy();
 };
 
 class IpuRegister_Bp : public SizedWordRegister {
