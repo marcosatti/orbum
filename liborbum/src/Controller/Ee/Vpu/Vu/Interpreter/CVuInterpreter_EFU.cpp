fe1cf185723d134e0057b7b2f210eae87a0c255a
hch12907 <haochengho12907@gmail.com>
2018-08-18 23:40:33 +0800

    Implement VU EFU instructions

--- liborbum/src/Controller/Ee/Vpu/Vu/Interpreter/CVuInterpreter_EFU.cpp
+++ liborbum/src/Controller/Ee/Vpu/Vu/Interpreter/CVuInterpreter_EFU.cpp
@@ -1,134 +1,187 @@
 #include <boost/format.hpp>
+#include <cmath>
 
 #include "Controller/Ee/Vpu/Vu/Interpreter/CVuInterpreter.hpp"
 #include "Core.hpp"
 
 void CVuInterpreter::ESADD(VuUnit_Base* unit, const VuInstruction inst)
 {
-    // TODO: Implement.
-#if defined(BUILD_DEBUG)
-    BOOST_LOG(Core::get_logger()) << boost::format("(%s, %d) ESADD: Not implemented.") % __FILENAME__ % __LINE__;
-#else
-    throw std::runtime_error("ESADD: Not implemented.");
-#endif
+    // P = VF[fs](x)^2 + VF[fs](y)^2 + VF[fs](z)^2
+
+    SizedQwordRegister& reg_source = unit->vf[inst.fs()];
+    SizedWordRegister& reg_dest = unit->p;
+
+    const f32 a = reg_source.read_float(VuVectorField::X);
+    const f32 b = reg_source.read_float(VuVectorField::Y);
+    const f32 c = reg_source.read_float(VuVectorField::Z);
+    const f32 result = (a * a) + (b * b) + (c * c);
+
+    reg_dest.write_float(result);
 }
 
 void CVuInterpreter::ERSADD(VuUnit_Base* unit, const VuInstruction inst)
 {
-    // TODO: Implement.
-#if defined(BUILD_DEBUG)
-    BOOST_LOG(Core::get_logger()) << boost::format("(%s, %d) ERSADD: Not implemented.") % __FILENAME__ % __LINE__;
-#else
-    throw std::runtime_error("ERSADD: Not implemented.");
-#endif
+    // P = 1 / (VF[fs](x)^2 + VF[fs](y)^2 + VF[fs](z)^2)
+
+    SizedQwordRegister& reg_source = unit->vf[inst.fs()];
+    SizedWordRegister& reg_dest = unit->p;
+
+    const f32 a = reg_source.read_float(VuVectorField::X);
+    const f32 b = reg_source.read_float(VuVectorField::Y);
+    const f32 c = reg_source.read_float(VuVectorField::Z);
+    const f32 result = 1.0f / ((a * a) + (b * b) + (c * c));
+
+    reg_dest.write_float(result);
 }
 
 void CVuInterpreter::ELENG(VuUnit_Base* unit, const VuInstruction inst)
 {
-    // TODO: Implement.
-#if defined(BUILD_DEBUG)
-    BOOST_LOG(Core::get_logger()) << boost::format("(%s, %d) ELENG: Not implemented.") % __FILENAME__ % __LINE__;
-#else
-    throw std::runtime_error("ELENG: Not implemented.");
-#endif
+    // P = sqrt(VF[fs](x)^2 + VF[fs](y)^2 + VF[fs](z)^2)
+
+    SizedQwordRegister& reg_source = unit->vf[inst.fs()];
+    SizedWordRegister& reg_dest = unit->p;
+
+    const f32 a = reg_source.read_float(VuVectorField::X);
+    const f32 b = reg_source.read_float(VuVectorField::Y);
+    const f32 c = reg_source.read_float(VuVectorField::Z);
+    const f32 result = std::sqrt((a * a) + (b * b) + (c * c));
+
+    reg_dest.write_float(result);
 }
 
 void CVuInterpreter::ERLENG(VuUnit_Base* unit, const VuInstruction inst)
 {
-    // TODO: Implement.
-#if defined(BUILD_DEBUG)
-    BOOST_LOG(Core::get_logger()) << boost::format("(%s, %d) ERLENG: Not implemented.") % __FILENAME__ % __LINE__;
-#else
-    throw std::runtime_error("ERLENG: Not implemented.");
-#endif
+    // P = 1 / sqrt(VF[fs](x)^2 + VF[fs](y)^2 + VF[fs](z)^2)
+
+    SizedQwordRegister& reg_source = unit->vf[inst.fs()];
+    SizedWordRegister& reg_dest = unit->p;
+
+    const f32 a = reg_source.read_float(VuVectorField::X);
+    const f32 b = reg_source.read_float(VuVectorField::Y);
+    const f32 c = reg_source.read_float(VuVectorField::Z);
+    const f32 result = 1.0f / std::sqrt((a * a) + (b * b) + (c * c));
+
+    reg_dest.write_float(result);
 }
 
 void CVuInterpreter::EATANxy(VuUnit_Base* unit, const VuInstruction inst)
 {
-    // TODO: Implement.
-#if defined(BUILD_DEBUG)
-    BOOST_LOG(Core::get_logger()) << boost::format("(%s, %d) EATANxy: Not implemented.") % __FILENAME__ % __LINE__;
-#else
-    throw std::runtime_error("EATANxy: Not implemented.");
-#endif
+    // P = arctan(VF[fs](y) / VF[fs](x))
+
+    SizedQwordRegister& reg_source = unit->vf[inst.fs()];
+    SizedWordRegister& reg_dest = unit->p;
+
+    const f32 a = reg_source.read_float(VuVectorField::X);
+    const f32 b = reg_source.read_float(VuVectorField::Y);
+    const f32 result = std::atan(b / a);
+
+    reg_dest.write_float(result);
 }
 
 void CVuInterpreter::EATANxz(VuUnit_Base* unit, const VuInstruction inst)
 {
-    // TODO: Implement.
-#if defined(BUILD_DEBUG)
-    BOOST_LOG(Core::get_logger()) << boost::format("(%s, %d) EATANxz: Not implemented.") % __FILENAME__ % __LINE__;
-#else
-    throw std::runtime_error("EATANxz: Not implemented.");
-#endif
+    // P = arctan(VF[fs](y) / VF[fs](x))
+
+    SizedQwordRegister& reg_source = unit->vf[inst.fs()];
+    SizedWordRegister& reg_dest = unit->p;
+
+    const f32 a = reg_source.read_float(VuVectorField::X);
+    const f32 b = reg_source.read_float(VuVectorField::Z);
+    const f32 result = std::atan(b / a);
+
+    reg_dest.write_float(result);
 }
 
 void CVuInterpreter::ESUM(VuUnit_Base* unit, const VuInstruction inst)
 {
-    // TODO: Implement.
-#if defined(BUILD_DEBUG)
-    BOOST_LOG(Core::get_logger()) << boost::format("(%s, %d) ESUM: Not implemented.") % __FILENAME__ % __LINE__;
-#else
-    throw std::runtime_error("ESUM: Not implemented.");
-#endif
+    // P = VF[fs](w) + VF[fs](x) + VF[fs](y) + VF[fs](z)
+
+    SizedQwordRegister& reg_source = unit->vf[inst.fs()];
+    SizedWordRegister& reg_dest = unit->p;
+
+    const f32 a = reg_source.read_float(VuVectorField::X);
+    const f32 b = reg_source.read_float(VuVectorField::Y);
+    const f32 c = reg_source.read_float(VuVectorField::Z);
+    const f32 d = reg_source.read_float(VuVectorField::W);
+    const f32 result = a + b + c + d;
+
+    reg_dest.write_float(result);
 }
 
 void CVuInterpreter::ERCPR(VuUnit_Base* unit, const VuInstruction inst)
 {
-    // TODO: Implement.
-#if defined(BUILD_DEBUG)
-    BOOST_LOG(Core::get_logger()) << boost::format("(%s, %d) ERCPR: Not implemented.") % __FILENAME__ % __LINE__;
-#else
-    throw std::runtime_error("ERCPR: Not implemented.");
-#endif
+    // P = 1 / VF[fs](fsf)
+
+    SizedQwordRegister& reg_source = unit->vf[inst.fs()];
+    SizedWordRegister& reg_dest = unit->p;
+
+    const f32 a = reg_source.read_float(inst.fsf());
+    const f32 result = 1.0f / a;
+
+    reg_dest.write_float(result);
 }
 
 void CVuInterpreter::ESQRT(VuUnit_Base* unit, const VuInstruction inst)
 {
-    // TODO: Implement.
-#if defined(BUILD_DEBUG)
-    BOOST_LOG(Core::get_logger()) << boost::format("(%s, %d) ESQRT: Not implemented.") % __FILENAME__ % __LINE__;
-#else
-    throw std::runtime_error("ESQRT: Not implemented.");
-#endif
+    // P = sqrt(VF[fs](fsf))
+
+    SizedQwordRegister& reg_source = unit->vf[inst.fs()];
+    SizedWordRegister& reg_dest = unit->p;
+
+    const f32 a = reg_source.read_float(inst.fsf());
+    const f32 result = std::sqrt(a);
+
+    reg_dest.write_float(result);
 }
 
 void CVuInterpreter::ERSQRT(VuUnit_Base* unit, const VuInstruction inst)
 {
-    // TODO: Implement.
-#if defined(BUILD_DEBUG)
-    BOOST_LOG(Core::get_logger()) << boost::format("(%s, %d) ERSQRT: Not implemented.") % __FILENAME__ % __LINE__;
-#else
-    throw std::runtime_error("ERSQRT: Not implemented.");
-#endif
+    // P = 1 / sqrt(VF[fs](fsf))
+
+    SizedQwordRegister& reg_source = unit->vf[inst.fs()];
+    SizedWordRegister& reg_dest = unit->p;
+
+    const f32 a = reg_source.read_float(inst.fsf());
+    const f32 result = 1.0f / std::sqrt(a);
+
+    reg_dest.write_float(result);
 }
 
 void CVuInterpreter::ESIN(VuUnit_Base* unit, const VuInstruction inst)
 {
-    // TODO: Implement.
-#if defined(BUILD_DEBUG)
-    BOOST_LOG(Core::get_logger()) << boost::format("(%s, %d) ESIN: Not implemented.") % __FILENAME__ % __LINE__;
-#else
-    throw std::runtime_error("ESIN: Not implemented.");
-#endif
+    // P = sin(VF[fs](fsf))
+
+    SizedQwordRegister& reg_source = unit->vf[inst.fs()];
+    SizedWordRegister& reg_dest = unit->p;
+
+    const f32 a = reg_source.read_float(inst.fsf());
+    const f32 result = std::sin(a);
+
+    reg_dest.write_float(result);
 }
 
 void CVuInterpreter::EATAN(VuUnit_Base* unit, const VuInstruction inst)
 {
-    // TODO: Implement.
-#if defined(BUILD_DEBUG)
-    BOOST_LOG(Core::get_logger()) << boost::format("(%s, %d) EATAN: Not implemented.") % __FILENAME__ % __LINE__;
-#else
-    throw std::runtime_error("EATAN: Not implemented.");
-#endif
+    // P = arctan(VF[fs](fsf))
+
+    SizedQwordRegister& reg_source = unit->vf[inst.fs()];
+    SizedWordRegister& reg_dest = unit->p;
+
+    const f32 a = reg_source.read_float(inst.fsf());
+    const f32 result = std::atan(a);
+
+    reg_dest.write_float(result);
 }
 
 void CVuInterpreter::EEXP(VuUnit_Base* unit, const VuInstruction inst)
 {
-    // TODO: Implement.
-#if defined(BUILD_DEBUG)
-    BOOST_LOG(Core::get_logger()) << boost::format("(%s, %d) EEXP: Not implemented.") % __FILENAME__ % __LINE__;
-#else
-    throw std::runtime_error("EEXP: Not implemented.");
-#endif
+    // P = exp(-VF[fs](fsf))
+
+    SizedQwordRegister& reg_source = unit->vf[inst.fs()];
+    SizedWordRegister& reg_dest = unit->p;
+
+    const f32 a = reg_source.read_float(inst.fsf());
+    const f32 result = std::exp(-a);
+
+    reg_dest.write_float(result);
 }
